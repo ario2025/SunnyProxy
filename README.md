5ec5670de81110f8e2ad2dbcb95b254306c427fa
Aristóteles Machado <aristoteles.machado@portoseguro.com.br>
2019-04-19 23:47:45 -0300

    Weblogic Support

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # SunnyProxy
+
+SunnyProxy is a first minimalist proxy-like servlet to serve all requests where commonly blocked for some
+reason in common circunstances.
+
+Weblogic
+--------
+
+Add in args this values:
+-DUseSunHttpHandler=true
+-Dweblogic.ssl.JSSEEnabled=true
+-Dweblogic.security.SSL.enableJSSE=true
+-Dweblogic.security.SSL.verbose=true
+-Dweblogic.security.SSL.enable.renegotiation=true
+-Dsun.security.ssl.allowUnsafeRenegotiation=true
+
+For debug SSL certs:
+-Djavax.net.debug=ssl
+
+- ATTENTION!
+------------
+This project it's under development and are not working yet as expected, so, don't mind to
+use it in your production environment!
--- src/main/java/br/com/aristotelesmachado/SunnyProxy.java
+++ src/main/java/br/com/aristotelesmachado/SunnyProxy.java
@@ -1,35 +1,81 @@
 package br.com.aristotelesmachado;
 
 import java.io.*;
-import javax.servlet.*;
-import javax.servlet.http.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import javax.net.ssl.*;
+import javax.security.cert.CertificateException;
+import javax.security.cert.X509Certificate;
 import javax.servlet.http.*;
 import javax.servlet.ServletException;
 
-
 public class SunnyProxy extends HttpServlet {
-
-    private String message;
-
     public void init() throws ServletException {
-        // Do required initialization
-        message = "Hello World";
+        //System.setProperty("javax.net.ssl.trustStore", "/path/to/my/cacerts");
+        //System.setProperty("javax.net.ssl.trustStore","/tmp/keystore.jks");
+        //System.out.println( SunnyProxy.class.getClassLoader().getResource("src/main/resources/custom-cacerts.jks").toString() );
+        System.out.println("[LOGGER] test");
+        System.setProperty("javax.net.ssl.trustStore", "src/main/resources/custom-cacerts.jks" );
+        System.setProperty("javax.net.ssl.trustStorePassword","changeit");
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Set response content type
-        response.setContentType("text/html");
+        String url = request.getParameter("url");
+        int ignoressl = 0;
+        if ( request.getParameter("ignoressl") != null && request.getParameter("ignoressl").equals("1") ) { ignoressl = 1; } else { ignoressl = 0; }
 
-        // Actual logic goes here.
-        PrintWriter out = response.getWriter();
-        out.println("<h1>" + message + "</h1>");
+        if ( url!=null && !url.trim().equals("") ) {
+            HostnameVerifier allHostsValid = new HostnameVerifier() {
+                public boolean verify(String hostname, SSLSession session) {
+                    return true;
+                }
+            };
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+            HttpsURLConnection.setFollowRedirects(true);
+            HttpURLConnection.setFollowRedirects(true);
+
+            URL obj = new URL(url);
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+            con.setInstanceFollowRedirects(true);
+            con.setConnectTimeout(1500); // TCP open port.
+            con.setReadTimeout(18500);   // Application response.
+            con.setUseCaches(true);
+
+            con.setRequestMethod("GET");
+            con.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36");
+            int responseCode = con.getResponseCode();
+
+            response.setStatus( con.getResponseCode() );
+            response.setContentType( con.getContentType() );
+            response.setHeader( "Cache-Control", con.getHeaderField("Cache-Control") );
+            response.setHeader( "Pragma", con.getHeaderField("Pragma") );
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer responseBuffer = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                //responseBuffer.append(inputLine);
+                response.getWriter().write( inputLine );
+            }
+            in.close();
+        } else {
+            response.setStatus(405);
+            response.getWriter().write("Parameter needed not found.");
+            response.sendError(405,"Parameter needed not found.");
+        }
     }
 
-    public void destroy() {
-        // do nothing.
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String protocol = request.getProtocol();
+        response.getWriter().write("POST Method!");
+
+        //response.getWriter().write( SunnyProxy.class.getClassLoader().getResource("src/main/resources/custom-cacerts.jks").toString() );
     }
 
+    public void destroy() { }
 
 }
